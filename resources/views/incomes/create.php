a294a65c650cd46776b13f0eb0c45c813262605b
MarFueGit <maricelafuentes100@gmail.com>
2022-12-09 12:49:21 -0600

    se agrego vendor

--- resources/views/incomes/create.php
+++ resources/views/incomes/create.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/styles .css">
-    <link rel="stylesheet" href="./public/css/create.css">
+    <link rel="stylesheet" href="/public/css/styles.css">
+    <link rel="stylesheet" href="/public/css/create.css">
     <title>Agrega nuevo ingreso</title>
 </head>
 <body>
